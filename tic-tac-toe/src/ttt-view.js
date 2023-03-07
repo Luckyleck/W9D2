b7e23de42365ba881a759909c52aa5e9b91c5fd9
Kelly Zhou <yinglinzhou0@gmail.com>
2023-03-07 17:26:22 -0500

    final push

--- tic-tac-toe/src/ttt-view.js
+++ tic-tac-toe/src/ttt-view.js
@@ -1,7 +1,7 @@
 class View {
-  constructor(game, figure) {
+  constructor(game, el) {
     this.game = game;
-    this.figure = figure;
+    this.el = el;
 
     // also another way to automatically run setupBoard when refreshed
     this.setupBoard();
@@ -9,19 +9,34 @@ class View {
 
   setupBoard() {
     // appendChild takes in a creation of an HTML element
-    const ul = this.figure.appendChild(document.createElement("ul"))
+    const ul = this.el.appendChild(document.createElement("ul"))
     
-    for (let i = 0; i < 9; i++) {
-      const li = document.createElement("li")
-      ul.appendChild(li);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let pos = [i, j];
+        const li = document.createElement("li")
+        const box = ul.appendChild(li);
+        // const article = document.querySelector("li")
+        li.setAttribute("data-id",`${pos}`)
+      }
 
     }
 
   }
   
-  bindEvents() {}
+  bindEvents() {
 
-  handleClick(e) {}
+
+  }
+
+  handleClick(e) {
+    //e is event
+    const cell = e.target;
+    debugger
+    this.game.playMove([row, col]);
+    console.log("hi")
+
+  }
 
   makeMove(square) {}
 
